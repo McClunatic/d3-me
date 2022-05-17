7140931b3394df63ae3b9f19cab3ab194ee06912
Brian McClune <bpmcclune@gmail.com>
2022-05-17 13:11:09 -0400

    Add CORS support for client origin

--- server/server/main.py
+++ server/server/main.py
@@ -4,9 +4,19 @@ import math
 import time
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def get_coordinate():
