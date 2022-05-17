cd416a11164af76eea0c44d8232a16a24c269691
Brian McClune <bpmcclune@gmail.com>
2022-05-17 07:54:03 -0400

    Update chart and main for working static chart

--- src/chart.ts
+++ src/chart.ts
@@ -6,110 +6,81 @@ interface Margin {
   bottom: number
   left: number
 }
-type Datum = [Date, number]
-type XAccessor = (d: Datum) => Date
-type YAccessor = (d: Datum) => number
 
-type Selection = d3.Selection<Element, Datum[], Element, any>
-type GSelection = d3.Selection<SVGGElement, Datum[], Element, any>
+interface Config {
+  margin: Margin
+  width: number
+  height: number
+}
 
-export default function chart() {
-  let margin: Margin = {top: 30, right: 30, bottom: 30, left: 30},
-      width = 800,
-      height = 600,
-      xValue: XAccessor = function(d: Datum) { return d[0]; },
-      yValue: YAccessor = function(d: Datum) { return d[1]; },
-      xScale = d3.scaleUtc(),
-      yScale = d3.scaleLinear(),
-      xAxis = function(g: GSelection) {
-        g
-          .attr("transform", `translate(0,${height - margin.bottom})`)
-          .call(d3.axisBottom(xScale).ticks(width / 80).tickSizeOuter(0))
-      },
-      line = d3.line<Datum>().x(X).y(Y)
+export type Datum = [Date, number]
+
+export type Selection = d3.Selection<d3.BaseType, Datum[], HTMLElement, any>
+
+export function chart(config?: Config) {
+  let margin = config?.margin || {top: 30, right: 30, bottom: 30, left: 30}
+  let width = config?.width || 800
+  let height = config?.height || 600
+
+  let x = d3.scaleUtc()
+  let y = d3.scaleLinear()
 
   function my(selection: Selection) {
     selection.each(function(data: Datum[]) {
 
-      // Update the x-scale.
-      let xDomain = d3.extent(data, (d: Datum) => d[0])
-      xScale
-          .domain(xDomain.map(v => v!))
-          .range([0, width - margin.left - margin.right]);
+      // Update the scales.
+      x
+        .domain(d3.extent(data, (d: Datum) => d[0]).map(v => v!))
+        .range([0, width - margin.left - margin.right])
+      y
+        .domain(d3.extent(data, (d: Datum) => d[1]).map(v => v!))
+        .range([height - margin.top - margin.bottom, 0])
 
-      // Update the y-scale.
-      let yDomain = d3.extent(data, (d: Datum) => d[1])
-      yScale
-          .domain(yDomain.map(v => v!))
-          .range([height - margin.top - margin.bottom, 0]);
+      // Create the line.
+      let line = d3.line<Datum>()
+        .x(d => x(d[0]))
+        .y(d => y(d[1]))
 
-      // Select the svg element, if it exists.
-      var svg = d3.select(this).selectAll("svg").data([data]);
+      // Create the skeletal chart if necessary.
+      let gEnter = d3.select(this).selectAll("svg")
+          .data([data])
+          .enter()
+        .append("svg")
+        .append("g")
+      gEnter
+        .append("path")
+          .attr("class", "line")
+          .attr("fill", "none")
+          .attr("stroke", "black")
+      gEnter
+        .append("g")
+          .attr("class", "x axis")
+      gEnter
+        .append("g")
+          .attr("class", "y axis")
 
-      // Otherwise, create the skeletal chart.
-      var gEnter = svg.enter().append("svg").append("g");
-      gEnter.append("path").attr("class", "area");
-      gEnter.append("path").attr("class", "line");
-      gEnter.append("g").attr("class", "x axis");
-
-      // Update the outer dimensions.
-      svg .attr("width", width)
-          .attr("height", height);
+      // Update the out dimensions.
+      let svg = d3.select(this).select("svg")
+          .attr("width", width)
+          .attr("height", height)
 
       // Update the inner dimensions.
-      var g = svg.select("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      let g = svg.select("g")
+          .attr("transform", `translate(${margin.left},${margin.top})`)
 
       // Update the line path.
       g.select(".line")
-          .attr("d", line);
+          .attr("d", line(data))
 
-      // Update the x-axis.
+      // Update the axes.
       g.select<SVGGElement>(".x.axis")
-          .attr("transform", "translate(0," + yScale.range()[0] + ")")
-          .call(xAxis);
-    });
+          .attr("transform", `translate(0,${y.range()[0]})`)
+          .call(d3.axisBottom(x).ticks(5))
+      g.select<SVGGElement>(".y.axis")
+          .attr("transform", `translate(${x.range()[0]},0)`)
+          .call(d3.axisLeft(y).ticks(5))
+    })
   }
 
-  // The x-accessor for the path generator; xScale ∘ xValue.
-  function X(d: Datum) {
-    return xScale(d[0]);
-  }
-
-  // The x-accessor for the path generator; yScale ∘ yValue.
-  function Y(d: Datum) {
-    return yScale(d[1]);
-  }
-
-  my.margin = function(value: Margin | undefined) {
-    if (!arguments.length) return margin;
-    margin = value!;
-    return my;
-  };
-
-  my.width = function(value: number | undefined) {
-    if (!arguments.length) return width;
-    width = value!;
-    return my;
-  };
-
-  my.height = function(value: number | undefined) {
-    if (!arguments.length) return height;
-    height = value!;
-    return my;
-  };
-
-  my.x = function(value: XAccessor | undefined) {
-    if (!arguments.length) return xValue;
-    xValue = value!;
-    return my;
-  };
-
-  my.y = function(value: YAccessor | undefined) {
-    if (!arguments.length) return yValue;
-    yValue = value!;
-    return my;
-  };
-
-  return my;
+  return my
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as d3 from 'd3'
+import { chart } from './chart'
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!
@@ -10,109 +11,17 @@ app.innerHTML = `
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
 
-function chart(config: Object) {
-  let width = 800
-  let height = 600
-  let margin = 30
-
-  function my() {
-
-  }
-
-  my.width = function(value: number) {
-    if (!arguments.length)
-      return width
-    width = value
-    return my
-  }
-
-  my.height = function(value: number) {
-    if (!arguments.length)
-      return height
-    height = value
-    return my
-  }
-
-  my.margin = function(value: number) {
-    if (!arguments.length)
-      return margin
-    margin = value
-    return my
-  }
+function formatData(data: d3.DSVRowArray<string>) {
+  let formatted: Array<[Date, number]> = data.map(
+    d => [d3.timeParse("%b %Y")(d.date!)!, parseFloat(d.price!)]
+  )
+  // formatted.forEach(val => console.log(val))
+  return formatted
 }
 
-const x: Array<number> = []
-for (let i = 0; i < 41; i++) {
-  x.push(i * 0.25)
-}
-const data: Array<[number, number]> = x.map(i => [i, Math.sin(i)])
-
-const fx = d3.scaleLinear()
-  .domain(<[number, number]>d3.extent(x))
-  .range([30, 770])
-
-const fy = d3.scaleLinear()
-  .domain([-1, 1])
-  .range([570, 30])
-
-let svg = d3.select("#app").append("svg")
-  .attr("width", 800)
-  .attr("height", 600)
-
-let gx = svg.append("g")
-    .attr("transform", "translate(0,570)")
-    .call(d3.axisBottom(fx))
-
-svg.append("g")
-    .attr("transform", "translate(30,0)")
-    .call(d3.axisLeft(fy))
-
-let g = svg.append("g")
-    .attr("fill", "none")
-    .attr("stroke-linecap", "round")
-g.selectAll("path")
-  .data(data)
-  .join("path")
-    .attr("d", d => `M${fx(d[0])},${fy(d[1])}h0`)
-    .attr("stroke", "#1F77B4")
-    .attr("stroke-width", 5)
-
-function update() {
-  let extent = <[number, number]>d3.extent(data.map(d => d[0]))
-  extent[1] += 0.25
-  data.push([extent[1], Math.sin(extent[1])])
-
-  fx.domain(extent)
-  gx.transition()
-    .duration(900)
-    .call(d3.axisBottom(fx))
-
-  g.selectAll("path")
-    .data(data, ((d: [number, number]) => d[0]) as d3.ValueFn<d3.BaseType, unknown, d3.KeyType>)
-    .join(
-      enter => enter.append("path")
-          .attr("d", d => `M${fx(d[0] + 0.25)},${fy(d[1])}h0`)
-          .attr("stroke-width", 5)
-        .call(enter => enter.transition()
-          .duration(900)
-          .attr("d", d => `M${fx(d[0])},${fy(d[1])}h0`)
-          .attr("stroke", "#1F77B4")
-        ),
-      update => update
-        .call(enter => enter.transition()
-          .duration(900)
-          .attr("d", d => `M${fx(d[0])},${fy(d[1])}h0`)
-        ),
-    )
-
-    .join("path")
-      .attr("stroke", "#1F77B4")
-      .attr("stroke-width", 5)
-
-  data.shift()
-}
-
-while (true) {
-  update()
-  await new Promise(r => setTimeout(r, 2000));
-}
+d3.csv("sp500.csv", ).then(formatData).then(data => {
+  let myChart = chart()
+  d3.select("#app")
+      .data([data])
+      .call(myChart)
+});
